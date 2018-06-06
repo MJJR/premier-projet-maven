267e5d9b3ff3eaab95cbaefd18e0b3aa10a7c5a6
Redor <matthieu.redor@hotmail.fr>
2018-06-06 15:00:09 +0200

    TP2 Profils fini

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -21,6 +21,10 @@ public class App
     	// using default font standard.flf, obtained from maven artifact
         String asciiArt1 = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt1);
+        
+        //affichage de l'envoronnement
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
     	
     }
 }
